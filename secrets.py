38f45ab09ee4723c2a487a994fc03c8a9dec471a
pandrei7 <preda.andrei174@gmail.com>
2018-01-11 21:29:20 +0200

    Add secrets

---
+++ secrets.py
@@ -0,0 +1,2 @@
+FILE_CRYPT_KEY = "R5zDuzSfahgzSt8W~i]Gze^zdDw6eqH2L&foyhBj2ncAtbdnjymIy7Z'EzSoUvF{Cz8L(DtGqm[kcp]3Yg"
+FIELD_CRYPT_KEY = 'D1fClnCoF(Zuw~7N~fhPyekV!rx6qcRtbym[jyc`6g_bqbyE giW{5r"PqegezCfKq1aks~IcKfv~br8xgFuFmW Xpe]5NwFjW%k`lgon3Sh'
