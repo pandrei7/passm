2fda2dbe39ca6320c33d27f8360a016d2a780fe9
pandrei7 <preda.andrei174@gmail.com>
2018-01-11 21:49:03 +0200

    Add random password generation to secrets.py

--- secrets.py
+++ secrets.py
@@ -1,2 +1,14 @@
+import random
+import string
+
 FILE_CRYPT_KEY = "R5zDuzSfahgzSt8W~i]Gze^zdDw6eqH2L&foyhBj2ncAtbdnjymIy7Z'EzSoUvF{Cz8L(DtGqm[kcp]3Yg"
 FIELD_CRYPT_KEY = 'D1fClnCoF(Zuw~7N~fhPyekV!rx6qcRtbym[jyc`6g_bqbyE giW{5r"PqegezCfKq1aks~IcKfv~br8xgFuFmW Xpe]5NwFjW%k`lgon3Sh'
+
+
+def random_password(length=64, lower=True, upper=True, digits=True, punct=True):
+  chars = ''
+  chars += string.ascii_lowercase if lower else ''
+  chars += string.ascii_uppercase if upper else ''
+  chars += string.digits if digits else ''
+  chars += string.punctuation if punct else ''
+  return ''.join(random.choices(chars, k=length))
