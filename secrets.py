385a621714ae139d8ad4efc8c7958293a3ffbb58
pandrei7 <preda.andrei174@gmail.com>
2018-01-11 23:43:27 +0200

    Fix secrets.py encryption-key usage

--- secrets.py
+++ secrets.py
@@ -1,11 +1,20 @@
 from binascii import hexlify
 
+import esocrypt as es
 import random
 import simplecrypt as sc
 import string
 
 FILE_CRYPT_KEY = "R5zDuzSfahgzSt8W~i]Gze^zdDw6eqH2L&foyhBj2ncAtbdnjymIy7Z'EzSoUvF{Cz8L(DtGqm[kcp]3Yg"
 FIELD_CRYPT_KEY = 'D1fClnCoF(Zuw~7N~fhPyekV!rx6qcRtbym[jyc`6g_bqbyE giW{5r"PqegezCfKq1aks~IcKfv~br8xgFuFmW Xpe]5NwFjW%k`lgon3Sh'
+
+
+def file_crypt_key():
+  return es.decrypt(FILE_CRYPT_KEY)
+
+
+def field_crypt_key():
+  return es.decrypt(FIELD_CRYPT_KEY)
 
 
 def random_password(length=64, lower=True, upper=True, digits=True, punct=True):
@@ -19,5 +28,5 @@ def random_password(length=64, lower=True, upper=True, digits=True, punct=True):
 
 def encrypt_field(data):
   text = data.encode('utf8')
-  text = sc.encrypt(FIELD_CRYPT_KEY, text)
+  text = sc.encrypt(field_crypt_key(), text)
   return str(hexlify(text))
