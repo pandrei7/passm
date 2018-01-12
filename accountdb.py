25fadca195ad95941abcd9015dc40e3638df1401
pandrei7 <preda.andrei174@gmail.com>
2018-01-12 14:53:53 +0200

    Add accountdb.py

---
+++ accountdb.py
@@ -0,0 +1,53 @@
+import sqlite3
+import userdb
+
+class AccountExistsException(Exception):
+  def __init__(self, name):
+    Exception.__init__(self, 'Account with name "' + name + '" already exists')
+
+
+def get_db_path(us):
+  return userdb.DB_DIR + '/' + us.name + '.db'
+
+
+def create_database(us):
+  path = get_db_path(us)
+  with sqlite3.connect(path) as conn:
+    c = conn.cursor()
+    c.execute('''CREATE TABLE accounts
+                 (name text, email text, username text, password text);''')
+
+
+def get_accounts_by_name_exact(us, name):
+  path = get_db_path(us)
+  with sqlite3.connect(path) as conn:
+    c = conn.cursor()
+    c.execute('SELECT * FROM accounts WHERE name LIKE ?;', (name,))
+    return c.fetchall()
+
+
+def get_accounts_by_name(us, name):
+  query_string = '%' + name + '%'
+  return get_accounts_by_name_exact(us, query_string)
+
+
+def insert_account(us, acc):
+  present = get_accounts_by_name(us, acc.name)
+  if len(present) > 0:
+    raise AccountExistsException(acc.name)
+
+  path = get_db_path(us)
+  with sqlite3.connect(path) as conn:
+    c = conn.cursor()
+    c.execute('INSERT INTO accounts VALUES(?,?,?,?);', acc.db_data())
+    conn.commit()
+
+
+def delete_account(us, acc):
+  path = get_db_path(us)
+  with sqlite3.connect(path) as conn:
+    c = conn.cursor()
+    c.execute('''DELETE FROM accounts
+                 WHERE name=? AND email=?
+                 AND username=? AND password=?;''', acc.db_data())
+    conn.commit()
