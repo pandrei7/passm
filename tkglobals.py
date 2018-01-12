9a2598f943e7d9c373d7b627438871dc382c753b
pandrei7 <preda.andrei174@gmail.com>
2018-01-12 22:37:21 +0200

    Add new fonts

--- tkglobals.py
+++ tkglobals.py
@@ -4,5 +4,13 @@ def regular_font():
   return font.Font(family='Helvetica', size=14)
 
 
+def small_regular_font():
+  return font.Font(family='Helvetica', size=10)
+
+
 def title_font():
   return font.Font(family='Helvetica', size=18, weight='bold')
+
+
+def hyper_title_font():
+  return font.Font(family='Helvetica', size=22, weight='bold')
