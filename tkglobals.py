d9f7c57bfdec06121e65de45aca58105b994512b
pandrei7 <preda.andrei174@gmail.com>
2018-01-13 14:59:37 +0200

    Add font tuples for buttons (helper functions)

--- tkglobals.py
+++ tkglobals.py
@@ -3,14 +3,33 @@ from tkinter import font
 def regular_font():
   return font.Font(family='Helvetica', size=14)
 
+def regular_font_tuple():
+  return ('Helvetica', 14)
+
 
 def small_regular_font():
   return font.Font(family='Helvetica', size=10)
+
+def small_regular_font_tuple():
+  return ('Helvetica', 10)
+
+
+def bold_regular_font():
+  return font.Font(family='Helvetica', size=14, weight='bold')
+
+def bold_regular_font_tuple():
+  return ('Helvetica', 14, 'bold')
 
 
 def title_font():
   return font.Font(family='Helvetica', size=18, weight='bold')
 
+def title_font_tuple():
+  return ('Helvetica', 18, 'bold')
+
 
 def hyper_title_font():
   return font.Font(family='Helvetica', size=22, weight='bold')
+
+def hyper_title_font_tuple():
+  return ('Helvetica', 22, 'bold')
