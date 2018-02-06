38a8e5840bfbcafc2f2c1b2dc36bc33c4c1cf366
pandrei7 <preda.andrei174@gmail.com>
2018-02-06 23:13:26 +0200

    Refactor tkutils

--- tkutils.py
+++ tkutils.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
-def prepare_centering(container):
-  container.grid_rowconfigure(0, weight=1)
-  container.grid_rowconfigure(2, weight=1)
-  container.grid_columnconfigure(0, weight=1)
-  container.grid_columnconfigure(2, weight=1)
+def prepare_centering(cont):
+  cont.grid_rowconfigure(0, weight=1)
+  cont.grid_rowconfigure(2, weight=1)
+  cont.grid_columnconfigure(0, weight=1)
+  cont.grid_columnconfigure(2, weight=1)
 
