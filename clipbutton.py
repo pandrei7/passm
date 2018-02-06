bdb6d829d6b48628e360475d0245de61d854926d
Preda Andrei <preda.andrei174@gmail.com>
2018-02-06 10:18:51 +0200

    Add copy to clipboard button

---
+++ clipbutton.py
@@ -0,0 +1,34 @@
+from tkinter.messagebox import showinfo
+import tkinter as tk
+
+class ClipButton(tk.Button):
+    def __init__(self, parent, entry, pop_title=None, pop_message=None):
+        tk.Button.__init__(self, parent)
+        self.parent = parent
+        self.entry = entry
+
+        self.title = pop_title
+        if self.title is None:
+            self.title = 'Copiat!'
+
+        self.message = pop_message
+        if self.message is None:
+            self.message = ('Am copiat textul.\n'
+                            'Il poti lipi doar daca aplicatia este deschisa.')
+
+        self.image = tk.PhotoImage(file='images/clipboard.gif')
+        self.config(image=self.image)
+        self.config(command=self.click)
+
+    def pop_up(self):
+        showinfo(title=self.title, message=self.message)
+
+    def click(self):
+        r = tk.Tk()
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append(self.entry.get())
+        r.update()
+        r.destroy()
+
+        self.pop_up()
