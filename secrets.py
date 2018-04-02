ffa8d8f78b4540fccc4540b3da407d75e677034c
pandrei7 <preda.andrei174@gmail.com>
2018-04-02 22:45:52 +0300

    Add key derivation from password

--- secrets.py
+++ secrets.py
@@ -1,14 +1,25 @@
 from cryptography.fernet import Fernet
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+import base64
+
 import esocrypt as es
 import random
 import string
 
 FIELD_CRYPT_KEY = r'C8kQQtbFrOM!mf4SjdqU4p)K\iV9knyfG O!Wlrk2MPDfcSCduDT68L!v%yBR>KTe\1JNj3N4xPcALm2VXEUCyy6EvDW9aPu4'
+DERIVATION_SALT = r'i9f@zkx}mjivbe8OwzldwpmjhS(7O{bhxxN!RvT|2W"y-U"ZQpfF"3ZlzqS#nkBhAy3ChFub|roubAd7IskeurmxK~wZ2Pu'
 
 
 def field_crypt_key():
   return es.decrypt(FIELD_CRYPT_KEY)
+
+
+def derivation_salt():
+  return es.decrypt(DERIVATION_SALT)
 
 
 def random_fernet_key():
@@ -25,6 +36,18 @@ def random_password(length=64, lower=True, upper=True, digits=True, punct=True):
   if len(chars) <= 0:
     return ''
   return ''.join(random.choices(chars, k=length))
+
+def derive_fernet_key(password):
+  kdf = PBKDF2HMAC(
+      algorithm=hashes.SHA256(),
+      length=32,
+      salt=derivation_salt().encode('utf-8'),
+      iterations=100000,
+      backend=default_backend()
+  )
+
+  password = password.encode('utf-8')
+  return base64.urlsafe_b64encode(kdf.derive(password)).decode('utf-8')
 
 
 def encrypt_data(key, data):
