a063392a763fac400cf987cf80446b8a0732e38e
Preda Andrei <preda.andrei174@gmail.com>
2018-04-30 14:55:57 +0300

    Fix duplicate account detection

--- accountdb.py
+++ accountdb.py
@@ -5,15 +5,20 @@ import user
 import utils
 
 class AccountExistsException(Exception):
+  """ Signal that an account already exists when it should not. """
+
   def __init__(self, name):
+    """ Initialize the object. """
     Exception.__init__(self, 'Account with name "' + name + '" already exists')
 
 
 def get_db_path(us):
+  """ Return the absolute path of the user's database. """
   return utils.get_resource_path('dbs', us.name + '.db')
 
 
 def create_database(us):
+  """ Create the user's database if it does not exist. """
   path = get_db_path(us)
   with sqlite3.connect(path) as conn:
     c = conn.cursor()
@@ -22,6 +27,7 @@ def create_database(us):
 
 
 def delete_database(us):
+  """ Delete the user's database. """
   try:
     os.remove(get_db_path(us))
   except:
@@ -29,6 +35,7 @@ def delete_database(us):
 
 
 def get_accounts_by_name_exact(us, name):
+  """ Return all accounts which have the exact name. """
   path = get_db_path(us)
   with sqlite3.connect(path) as conn:
     c = conn.cursor()
@@ -37,17 +44,28 @@ def get_accounts_by_name_exact(us, name):
 
 
 def get_accounts_by_name(us, name):
+  """ Return all accounts whose names contain a word.
+
+  :param us: the User who owns the accounts
+  :param name: the word that is searched
+  """
   query_string = '%' + name + '%'
   return get_accounts_by_name_exact(us, query_string)
 
 
 def get_all_accounts(us):
+  """ Return all the accounts in the user's database. """
   return get_accounts_by_name(us, '')
 
 
 def insert_account(us, acc):
-  present = get_accounts_by_name(us, acc.name)
-  if len(present) > 0:
+  """ Insert a new account in the user's database.
+
+  :param us: the User who owns the database
+  :param acc: the new Account
+  :raises AccountExistsException: if the account already exists
+  """
+  if get_accounts_by_name_exact(us, acc.name):
     raise AccountExistsException(acc.name)
 
   path = get_db_path(us)
@@ -58,6 +76,7 @@ def insert_account(us, acc):
 
 
 def delete_account(us, acc):
+  """ Delete an account from the user's database. """
   path = get_db_path(us)
   with sqlite3.connect(path) as conn:
     c = conn.cursor()
@@ -68,6 +87,7 @@ def delete_account(us, acc):
 
 
 def update_account_data(us, acc):
+  """ Update an existing account's data. """
   path = get_db_path(us)
   with sqlite3.connect(path) as conn:
     c = conn.cursor()
@@ -78,8 +98,9 @@ def update_account_data(us, acc):
 
 
 def change_account(us, acc):
-  if len(get_accounts_by_name_exact(us, acc.name)) <= 0:
+  """ Change an existing account or insert a new one. """
+  try:
     insert_account(us, acc)
-  else:
+  except AccountExistsException:
     update_account_data(us, acc)
 
